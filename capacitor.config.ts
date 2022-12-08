83bcc27e2c19cf0ceece0af507116cca8e1dd93d
Damian Tarnawsky <damian@ionic.io>
2022-12-08 12:30:53 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'fw.2957',
+  appName: 'fw-2957',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
